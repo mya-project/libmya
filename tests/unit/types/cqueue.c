9c5807049c477c341c6a9337180faa4339b3852c
Luiz Felipe <silva97@freedev.com.br>
2025-01-15 17:56:09 -0300

    test: add tests to circular queue

---
+++ tests/unit/types/cqueue.c
@@ -0,0 +1,212 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "err.h"
+#include "queue.h"
+#include "unity.h"
+
+void
+test_cqueue_init(void)
+{
+  cqueue_t queue;
+
+  cqueue_init(&queue, 500);
+
+  TEST_ASSERT_EQUAL(0, queue.length);
+  TEST_ASSERT_EQUAL(0, queue._current);
+  TEST_ASSERT_EQUAL(-1, queue._last);
+  TEST_ASSERT_EQUAL(500, queue._max_length);
+  TEST_ASSERT_NOT_NULL(queue.data);
+
+  memset(queue.data, 0, sizeof(int) * 500);
+
+  free(queue.data);
+}
+
+void
+test_cqueue_add(void)
+{
+  cqueue_t queue;
+
+  cqueue_init(&queue, 5);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 1));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 2));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 3));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 4));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 5));
+
+  TEST_ASSERT_EQUAL_INT_ARRAY(((int[]){ 1, 2, 3, 4, 5 }), queue.data, 5);
+  TEST_ASSERT_EQUAL(5, queue.length);
+
+  TEST_ASSERT_EQUAL(ERR_MAX_SIZE_EXCEEDED, cqueue_add(&queue, 6));
+
+  free(queue.data);
+}
+
+void
+test_cqueue_get(void)
+{
+  cqueue_t queue;
+  int value;
+
+  cqueue_init(&queue, 3);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 1));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 2));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 3));
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(1, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(2, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(3, value);
+
+  TEST_ASSERT_EQUAL(0, queue.length);
+
+  TEST_ASSERT_EQUAL(ERR_EMPTY, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(3, value);  // Expects to not change the value.
+
+  free(queue.data);
+}
+
+void
+test_cqueue_lookup(void)
+{
+  cqueue_t queue;
+  int value = -1;
+
+  cqueue_init(&queue, 3);
+
+  TEST_ASSERT_EQUAL(ERR_EMPTY, cqueue_lookup(&queue, &value, 0));
+  TEST_ASSERT_EQUAL(-1, value);  // Expects to not change the value.
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 1));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 2));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 3));
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_lookup(&queue, &value, 0));
+  TEST_ASSERT_EQUAL(1, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_lookup(&queue, &value, 1));
+  TEST_ASSERT_EQUAL(2, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_lookup(&queue, &value, 2));
+  TEST_ASSERT_EQUAL(3, value);
+
+  TEST_ASSERT_EQUAL(ERR_INVALID_INDEX, cqueue_lookup(&queue, &value, 3));
+  TEST_ASSERT_EQUAL(3, value);  // Expects to not change the value.
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(1, value);
+
+  free(queue.data);
+}
+
+void
+test_cqueue_isempty(void)
+{
+  cqueue_t queue;
+  int value;
+
+  cqueue_init(&queue, 3);
+
+  TEST_ASSERT_TRUE(cqueue_isempty(&queue));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 1));
+  TEST_ASSERT_FALSE(cqueue_isempty(&queue));
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_TRUE(cqueue_isempty(&queue));
+}
+
+void
+test_cqueue_isfull(void)
+{
+  cqueue_t queue;
+  int value;
+
+  cqueue_init(&queue, 2);
+
+  TEST_ASSERT_FALSE(cqueue_isfull(&queue));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 1));
+  TEST_ASSERT_FALSE(cqueue_isfull(&queue));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 2));
+  TEST_ASSERT_TRUE(cqueue_isfull(&queue));
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_FALSE(cqueue_isfull(&queue));
+}
+
+void
+test_cqueue_circular_case(void)
+{
+  cqueue_t queue;
+  int value;
+
+  cqueue_init(&queue, 3);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 1));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 2));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 3));
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_lookup(&queue, &value, 1));
+  TEST_ASSERT_EQUAL(2, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(1, value);
+  TEST_ASSERT_EQUAL(1, queue._current);
+  TEST_ASSERT_EQUAL(2, queue._last);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_lookup(&queue, &value, 1));
+  TEST_ASSERT_EQUAL(3, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(2, value);
+  TEST_ASSERT_EQUAL(2, queue._current);
+  TEST_ASSERT_EQUAL(2, queue._last);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 4));
+  TEST_ASSERT_EQUAL(2, queue._current);
+  TEST_ASSERT_EQUAL(0, queue._last);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 5));
+  TEST_ASSERT_EQUAL(2, queue._current);
+  TEST_ASSERT_EQUAL(1, queue._last);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_lookup(&queue, &value, 1));
+  TEST_ASSERT_EQUAL(4, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(3, value);
+  TEST_ASSERT_EQUAL(0, queue._current);
+  TEST_ASSERT_EQUAL(1, queue._last);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_lookup(&queue, &value, 1));
+  TEST_ASSERT_EQUAL(5, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(4, value);
+  TEST_ASSERT_EQUAL(1, queue._current);
+  TEST_ASSERT_EQUAL(1, queue._last);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(5, value);
+  TEST_ASSERT_EQUAL(2, queue._current);
+  TEST_ASSERT_EQUAL(1, queue._last);
+
+  TEST_ASSERT_TRUE(cqueue_isempty(&queue));
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 1));
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_add(&queue, 2));
+  TEST_ASSERT_EQUAL(2, queue._current);
+  TEST_ASSERT_EQUAL(0, queue._last);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_lookup(&queue, &value, 1));
+  TEST_ASSERT_EQUAL(2, value);
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(1, value);
+  TEST_ASSERT_EQUAL(0, queue._current);
+  TEST_ASSERT_EQUAL(0, queue._last);
+
+  TEST_ASSERT_EQUAL(ERR_OK, cqueue_get(&queue, &value));
+  TEST_ASSERT_EQUAL(2, value);
+  TEST_ASSERT_EQUAL(1, queue._current);
+  TEST_ASSERT_EQUAL(0, queue._last);
+
+  TEST_ASSERT_TRUE(cqueue_isempty(&queue));
+}
--- tests/unit/types/test_types.c
+++ tests/unit/types/test_types.c
@@ -14,6 +14,14 @@ main(void)
   RUN_TEST(test_module_init_with_filepath_bigger_than_maximum_expects_to_truncate);
   RUN_TEST(test_module_add_token);
 
+  RUN_TEST(test_cqueue_init);
+  RUN_TEST(test_cqueue_add);
+  RUN_TEST(test_cqueue_get);
+  RUN_TEST(test_cqueue_lookup);
+  RUN_TEST(test_cqueue_isempty);
+  RUN_TEST(test_cqueue_isfull);
+  RUN_TEST(test_cqueue_circular_case);
+
   return UNITY_END();
 }
 
