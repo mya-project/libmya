ba59540f136ba974d63254ce5a1ab39032f6d1e7
Luiz Felipe <silva97@freedev.com.br>
2025-01-18 19:27:59 -0300

    test: add tests to AST functions

---
+++ tests/unit/types/test_ast.c
@@ -0,0 +1,70 @@
+#include "ast.h"
+#include "token.h"
+#include "unity.h"
+
+void
+test_ast_node_init(void)
+{
+  ast_node_t node;
+  token_t token;
+
+  token_init(&token, "+", TK_OPERATOR, 1, 2);
+
+  ast_node_init(&node, NULL, &token);
+
+  TEST_ASSERT_NULL(node.parent);
+  TEST_ASSERT_EQUAL(&token, node.token);
+  TEST_ASSERT_EQUAL(0, node.children_count);
+  TEST_ASSERT_EQUAL(AST_INITIAL_CHILDREN_LENGTH, node._children_length);
+
+  ast_close(&node);
+}
+
+void
+test_ast_add_children(void)
+{
+  ast_node_t root;
+  token_t root_token;
+  token_t child_token;
+
+  token_init(&root_token, "inst", TK_KEYWORD, 1, 1);
+  token_init(&child_token, "mov", TK_IDENTIFIER, 1, 5);
+
+  ast_node_init(&root, NULL, &root_token);
+
+  for (int i = 0; i < 300; i++) {
+    ast_add_children(&root, &child_token);
+
+    TEST_ASSERT_EQUAL(i + 1, root.children_count);
+    TEST_ASSERT_GREATER_OR_EQUAL(root.children_count, root._children_length);
+    TEST_ASSERT_EQUAL(&root, root.children[i].parent);
+    TEST_ASSERT_EQUAL(&child_token, root.children[i].token);
+  }
+
+  ast_close(&root);
+}
+
+/////
+
+int
+main(void)
+{
+  UNITY_BEGIN();
+
+  RUN_TEST(test_ast_node_init);
+  RUN_TEST(test_ast_add_children);
+
+  return UNITY_END();
+}
+
+void
+setUp(void)
+{
+  //
+}
+
+void
+tearDown(void)
+{
+  //
+}
