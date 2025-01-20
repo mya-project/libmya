973d734b95010362602d644d4aca8b02c513cab9
Luiz Felipe <silva97@freedev.com.br>
2025-01-19 22:59:49 -0300

    test: update tests to stack

--- tests/unit/types/test_stack.c
+++ tests/unit/types/test_stack.c
@@ -1,3 +1,4 @@
+#include "ast.h"
 #include "stack.h"
 #include "unity.h"
 
@@ -11,6 +12,8 @@ test_stack_init(void)
   TEST_ASSERT_EQUAL(0, stack.length);
   TEST_ASSERT_EQUAL(STACK_INITIAL_LENGTH, stack._size);
   TEST_ASSERT_NOT_NULL(stack.values);
+
+  stack_close(&stack);
 }
 
 void
@@ -19,28 +22,64 @@ test_stack_push_and_pop(void)
   stack_t stack;
   stack_init(&stack);
 
-  stack_t* push_value = &stack;
-  void* pop_value = NULL;
+  token_t* token = NULL;
+  ast_node_t pop_node;
 
   for (int i = 0; i < 300; i++) {
-    TEST_ASSERT_EQUAL(i, stack_push(&stack, push_value + i));
+    TEST_ASSERT_EQUAL(&stack.values[i], stack_push(&stack, NT_EXPRESSION, token + i));
 
     TEST_ASSERT_EQUAL(i + 1, stack.length);
     TEST_ASSERT_GREATER_OR_EQUAL(stack.length, stack._size);
   }
 
   for (int i = 300; i > 0; i--) {
-    TEST_ASSERT_EQUAL(ERR_OK, stack_pop(&stack, &pop_value));
+    TEST_ASSERT_EQUAL(ERR_OK, stack_pop(&stack, &pop_node));
 
     TEST_ASSERT_EQUAL(i - 1, stack.length);
-    TEST_ASSERT_EQUAL(push_value + i - 1, pop_value);
+    TEST_ASSERT_EQUAL(NT_EXPRESSION, pop_node.type);
+    TEST_ASSERT_EQUAL(token + i - 1, pop_node.token);
   }
 
   TEST_ASSERT_EQUAL(0, stack.length);
 
-  pop_value = NULL;
-  TEST_ASSERT_EQUAL(ERR_EMPTY, stack_pop(&stack, &pop_value));
-  TEST_ASSERT_NULL(pop_value);
+  TEST_ASSERT_EQUAL(ERR_EMPTY, stack_pop(&stack, &pop_node));
+
+  stack_close(&stack);
+}
+
+void
+test_stack_insert(void)
+{
+  stack_t stack;
+  stack_init(&stack);
+
+  token_t* token = NULL;
+  ast_node_t insert_node;
+  ast_node_t pop_node;
+
+  ast_node_init(&insert_node, NULL, NT_EXPRESSION, NULL);
+
+  for (int i = 0; i < 300; i++) {
+    insert_node.token = token + i;
+    TEST_ASSERT_EQUAL(&stack.values[i], stack_insert(&stack, &insert_node));
+
+    TEST_ASSERT_EQUAL(i + 1, stack.length);
+    TEST_ASSERT_GREATER_OR_EQUAL(stack.length, stack._size);
+  }
+
+  for (int i = 300; i > 0; i--) {
+    TEST_ASSERT_EQUAL(ERR_OK, stack_pop(&stack, &pop_node));
+
+    TEST_ASSERT_EQUAL(i - 1, stack.length);
+    TEST_ASSERT_EQUAL(NT_EXPRESSION, pop_node.type);
+    TEST_ASSERT_EQUAL(token + i - 1, pop_node.token);
+  }
+
+  TEST_ASSERT_EQUAL(0, stack.length);
+
+  TEST_ASSERT_EQUAL(ERR_EMPTY, stack_pop(&stack, &pop_node));
+
+  stack_close(&stack);
 }
 
 void
@@ -49,17 +88,18 @@ test_stack_isempty(void)
   stack_t stack;
   stack_init(&stack);
 
-  stack_t* push_value = &stack;
-  void* pop_value = NULL;
+  ast_node_t pop_node;
 
   TEST_ASSERT_TRUE(stack_isempty(&stack));
-  stack_push(&stack, push_value);
-  stack_push(&stack, push_value + 1);
+  stack_push(&stack, NT_EXPRESSION, NULL);
+  stack_push(&stack, NT_EXPRESSION, NULL);
   TEST_ASSERT_FALSE(stack_isempty(&stack));
-  stack_pop(&stack, &pop_value);
+  stack_pop(&stack, &pop_node);
   TEST_ASSERT_FALSE(stack_isempty(&stack));
-  stack_pop(&stack, &pop_value);
+  stack_pop(&stack, &pop_node);
   TEST_ASSERT_TRUE(stack_isempty(&stack));
+
+  stack_close(&stack);
 }
 
 /////
@@ -71,6 +111,7 @@ main(void)
 
   RUN_TEST(test_stack_init);
   RUN_TEST(test_stack_push_and_pop);
+  RUN_TEST(test_stack_insert);
   RUN_TEST(test_stack_isempty);
 
   return UNITY_END();
